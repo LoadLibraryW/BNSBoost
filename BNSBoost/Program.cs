1774c07cf7d4a750d03862d182e6bc953de0076a
Tudor <xyene@dmoj.ca>
2017-04-26 16:21:25 -0400

    Launch injector from UI

--- BNSBoost/Program.cs
+++ BNSBoost/Program.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace BNSBoost
 {
     static class Program
     {
+        [DllImport("Injector.dll")]
+        public static extern void Launch();
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            new Thread(Launch).Start();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
