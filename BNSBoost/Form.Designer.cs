810bdcbe20dcd2e52b752154837bab6b096ccb7b
LoadLibraryW <LoadLibraryW@protonmail.com>
2017-04-27 02:12:20 -0400

    Use fewer nested panels

--- BNSBoost/Form.Designer.cs
+++ BNSBoost/Form.Designer.cs
@@ -31,25 +31,22 @@
             this.LaunchButton = new System.Windows.Forms.Button();
             this.GameTabPage = new System.Windows.Forms.TabPage();
             this.GameTabPanel = new System.Windows.Forms.Panel();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.LauncherPathLabel = new System.Windows.Forms.Label();
-            this.LauncherPathTextBox = new System.Windows.Forms.TextBox();
             this.DisableTextureStreamingCheckbox = new System.Windows.Forms.CheckBox();
             this.UseAllCoresCheckbox = new System.Windows.Forms.CheckBox();
-            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
             this.MainTabControl = new System.Windows.Forms.TabControl();
+            this.LauncherPathTextBox = new System.Windows.Forms.TextBox();
+            this.LauncherPathLabel = new System.Windows.Forms.Label();
             this.GameTabPage.SuspendLayout();
             this.GameTabPanel.SuspendLayout();
-            this.tableLayoutPanel1.SuspendLayout();
             this.MainTabControl.SuspendLayout();
             this.SuspendLayout();
             // 
             // LaunchButton
             // 
             this.LaunchButton.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.LaunchButton.Location = new System.Drawing.Point(10, 264);
+            this.LaunchButton.Location = new System.Drawing.Point(10, 267);
             this.LaunchButton.Name = "LaunchButton";
-            this.LaunchButton.Size = new System.Drawing.Size(866, 70);
+            this.LaunchButton.Size = new System.Drawing.Size(912, 70);
             this.LaunchButton.TabIndex = 0;
             this.LaunchButton.Text = "Launch!";
             this.LaunchButton.UseVisualStyleBackColor = true;
@@ -61,7 +58,7 @@
             this.GameTabPage.Location = new System.Drawing.Point(10, 48);
             this.GameTabPage.Name = "GameTabPage";
             this.GameTabPage.Padding = new System.Windows.Forms.Padding(3);
-            this.GameTabPage.Size = new System.Drawing.Size(846, 196);
+            this.GameTabPage.Size = new System.Drawing.Size(892, 199);
             this.GameTabPage.TabIndex = 0;
             this.GameTabPage.Text = "Game";
             this.GameTabPage.UseVisualStyleBackColor = true;
@@ -69,59 +66,22 @@
             // GameTabPanel
             // 
             this.GameTabPanel.BackColor = System.Drawing.Color.Transparent;
-            this.GameTabPanel.Controls.Add(this.tableLayoutPanel1);
-            this.GameTabPanel.Controls.Add(this.flowLayoutPanel2);
+            this.GameTabPanel.Controls.Add(this.LauncherPathTextBox);
+            this.GameTabPanel.Controls.Add(this.UseAllCoresCheckbox);
+            this.GameTabPanel.Controls.Add(this.LauncherPathLabel);
+            this.GameTabPanel.Controls.Add(this.DisableTextureStreamingCheckbox);
             this.GameTabPanel.Dock = System.Windows.Forms.DockStyle.Fill;
             this.GameTabPanel.Location = new System.Drawing.Point(3, 3);
             this.GameTabPanel.Name = "GameTabPanel";
-            this.GameTabPanel.Size = new System.Drawing.Size(840, 190);
+            this.GameTabPanel.Size = new System.Drawing.Size(886, 193);
             this.GameTabPanel.TabIndex = 7;
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 1;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Controls.Add(this.LauncherPathLabel, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.LauncherPathTextBox, 0, 3);
-            this.tableLayoutPanel1.Controls.Add(this.DisableTextureStreamingCheckbox, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.UseAllCoresCheckbox, 0, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 4;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(840, 190);
-            this.tableLayoutPanel1.TabIndex = 9;
-            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
-            // 
-            // LauncherPathLabel
-            // 
-            this.LauncherPathLabel.AutoSize = true;
-            this.LauncherPathLabel.Dock = System.Windows.Forms.DockStyle.Left;
-            this.LauncherPathLabel.Location = new System.Drawing.Point(3, 84);
-            this.LauncherPathLabel.Name = "LauncherPathLabel";
-            this.LauncherPathLabel.Size = new System.Drawing.Size(205, 32);
-            this.LauncherPathLabel.TabIndex = 14;
-            this.LauncherPathLabel.Text = "Launcher path:";
-            this.LauncherPathLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
-            // 
-            // LauncherPathTextBox
-            // 
-            this.LauncherPathTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.LauncherPathTextBox.Location = new System.Drawing.Point(3, 119);
-            this.LauncherPathTextBox.Name = "LauncherPathTextBox";
-            this.LauncherPathTextBox.Size = new System.Drawing.Size(834, 38);
-            this.LauncherPathTextBox.TabIndex = 15;
             // 
             // DisableTextureStreamingCheckbox
             // 
             this.DisableTextureStreamingCheckbox.AutoSize = true;
             this.DisableTextureStreamingCheckbox.Checked = true;
             this.DisableTextureStreamingCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.DisableTextureStreamingCheckbox.Location = new System.Drawing.Point(3, 45);
+            this.DisableTextureStreamingCheckbox.Location = new System.Drawing.Point(9, 45);
             this.DisableTextureStreamingCheckbox.Name = "DisableTextureStreamingCheckbox";
             this.DisableTextureStreamingCheckbox.Size = new System.Drawing.Size(375, 36);
             this.DisableTextureStreamingCheckbox.TabIndex = 2;
@@ -133,22 +93,12 @@
             this.UseAllCoresCheckbox.AutoSize = true;
             this.UseAllCoresCheckbox.Checked = true;
             this.UseAllCoresCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.UseAllCoresCheckbox.Location = new System.Drawing.Point(3, 3);
+            this.UseAllCoresCheckbox.Location = new System.Drawing.Point(9, 3);
             this.UseAllCoresCheckbox.Name = "UseAllCoresCheckbox";
             this.UseAllCoresCheckbox.Size = new System.Drawing.Size(216, 36);
             this.UseAllCoresCheckbox.TabIndex = 2;
             this.UseAllCoresCheckbox.Text = "Use all cores";
             this.UseAllCoresCheckbox.UseVisualStyleBackColor = true;
-            // 
-            // flowLayoutPanel2
-            // 
-            this.flowLayoutPanel2.AutoSize = true;
-            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
-            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 0);
-            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
-            this.flowLayoutPanel2.Size = new System.Drawing.Size(840, 0);
-            this.flowLayoutPanel2.TabIndex = 8;
             // 
             // MainTabControl
             // 
@@ -157,15 +107,34 @@
             this.MainTabControl.Location = new System.Drawing.Point(10, 10);
             this.MainTabControl.Name = "MainTabControl";
             this.MainTabControl.SelectedIndex = 0;
-            this.MainTabControl.Size = new System.Drawing.Size(866, 254);
+            this.MainTabControl.Size = new System.Drawing.Size(912, 257);
             this.MainTabControl.TabIndex = 7;
+            // 
+            // LauncherPathTextBox
+            // 
+            this.LauncherPathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.LauncherPathTextBox.Location = new System.Drawing.Point(9, 129);
+            this.LauncherPathTextBox.Name = "LauncherPathTextBox";
+            this.LauncherPathTextBox.Size = new System.Drawing.Size(855, 38);
+            this.LauncherPathTextBox.TabIndex = 15;
+            // 
+            // LauncherPathLabel
+            // 
+            this.LauncherPathLabel.AutoSize = true;
+            this.LauncherPathLabel.Location = new System.Drawing.Point(3, 94);
+            this.LauncherPathLabel.Name = "LauncherPathLabel";
+            this.LauncherPathLabel.Size = new System.Drawing.Size(205, 32);
+            this.LauncherPathLabel.TabIndex = 18;
+            this.LauncherPathLabel.Text = "Launcher path:";
+            this.LauncherPathLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             // 
             // BNSBoostForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(886, 344);
+            this.ClientSize = new System.Drawing.Size(932, 347);
             this.Controls.Add(this.MainTabControl);
             this.Controls.Add(this.LaunchButton);
             this.Name = "BNSBoostForm";
@@ -175,8 +144,6 @@
             this.GameTabPage.ResumeLayout(false);
             this.GameTabPanel.ResumeLayout(false);
             this.GameTabPanel.PerformLayout();
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
             this.MainTabControl.ResumeLayout(false);
             this.ResumeLayout(false);
 
@@ -188,12 +155,10 @@
         private System.Windows.Forms.TabPage GameTabPage;
         private System.Windows.Forms.TabControl MainTabControl;
         private System.Windows.Forms.Panel GameTabPanel;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Label LauncherPathLabel;
-        private System.Windows.Forms.TextBox LauncherPathTextBox;
         private System.Windows.Forms.CheckBox DisableTextureStreamingCheckbox;
         private System.Windows.Forms.CheckBox UseAllCoresCheckbox;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label LauncherPathLabel;
+        private System.Windows.Forms.TextBox LauncherPathTextBox;
     }
 }
 
