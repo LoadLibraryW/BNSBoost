ce8bc862cb1e1e08f943aa4989a49b507b6b8f25
Tudor <xyene@dmoj.ca>
2017-04-28 18:08:36 -0400

    Configure VC to build Release properly

--- BNSBoost/Form.Designer.cs
+++ BNSBoost/Form.Designer.cs
@@ -47,10 +47,10 @@
             // LaunchButton
             // 
             this.LaunchButton.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.LaunchButton.Location = new System.Drawing.Point(4, 192);
-            this.LaunchButton.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.LaunchButton.Location = new System.Drawing.Point(11, 458);
+            this.LaunchButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.LaunchButton.Name = "LaunchButton";
-            this.LaunchButton.Size = new System.Drawing.Size(351, 29);
+            this.LaunchButton.Size = new System.Drawing.Size(935, 69);
             this.LaunchButton.TabIndex = 0;
             this.LaunchButton.Text = "Launch!";
             this.LaunchButton.UseVisualStyleBackColor = true;
@@ -59,11 +59,11 @@
             // GameTabPage
             // 
             this.GameTabPage.Controls.Add(this.GameTabPanel);
-            this.GameTabPage.Location = new System.Drawing.Point(4, 22);
-            this.GameTabPage.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.GameTabPage.Location = new System.Drawing.Point(10, 48);
+            this.GameTabPage.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.GameTabPage.Name = "GameTabPage";
-            this.GameTabPage.Padding = new System.Windows.Forms.Padding(1, 1, 1, 1);
-            this.GameTabPage.Size = new System.Drawing.Size(343, 162);
+            this.GameTabPage.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.GameTabPage.Size = new System.Drawing.Size(915, 390);
             this.GameTabPage.TabIndex = 0;
             this.GameTabPage.Text = "Game";
             this.GameTabPage.UseVisualStyleBackColor = true;
@@ -79,10 +79,10 @@
             this.GameTabPanel.Controls.Add(this.LauncherPathLabel);
             this.GameTabPanel.Controls.Add(this.DisableTextureStreamingCheckbox);
             this.GameTabPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.GameTabPanel.Location = new System.Drawing.Point(1, 1);
-            this.GameTabPanel.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.GameTabPanel.Location = new System.Drawing.Point(3, 2);
+            this.GameTabPanel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.GameTabPanel.Name = "GameTabPanel";
-            this.GameTabPanel.Size = new System.Drawing.Size(341, 160);
+            this.GameTabPanel.Size = new System.Drawing.Size(909, 386);
             this.GameTabPanel.TabIndex = 7;
             // 
             // GameDirectoryPathTextBox
@@ -90,20 +90,19 @@
             this.GameDirectoryPathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.GameDirectoryPathTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::BNSBoost.Properties.Settings.Default, "GameDirectoryPath", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.GameDirectoryPathTextBox.Location = new System.Drawing.Point(3, 117);
-            this.GameDirectoryPathTextBox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.GameDirectoryPathTextBox.Location = new System.Drawing.Point(8, 279);
+            this.GameDirectoryPathTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.GameDirectoryPathTextBox.Name = "GameDirectoryPathTextBox";
-            this.GameDirectoryPathTextBox.Size = new System.Drawing.Size(333, 20);
+            this.GameDirectoryPathTextBox.Size = new System.Drawing.Size(881, 38);
             this.GameDirectoryPathTextBox.TabIndex = 21;
             this.GameDirectoryPathTextBox.Text = global::BNSBoost.Properties.Settings.Default.GameDirectoryPath;
             // 
             // GameDirectoryPathLabel
             // 
             this.GameDirectoryPathLabel.AutoSize = true;
-            this.GameDirectoryPathLabel.Location = new System.Drawing.Point(0, 102);
-            this.GameDirectoryPathLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.GameDirectoryPathLabel.Location = new System.Drawing.Point(0, 243);
             this.GameDirectoryPathLabel.Name = "GameDirectoryPathLabel";
-            this.GameDirectoryPathLabel.Size = new System.Drawing.Size(105, 13);
+            this.GameDirectoryPathLabel.Size = new System.Drawing.Size(279, 32);
             this.GameDirectoryPathLabel.TabIndex = 20;
             this.GameDirectoryPathLabel.Text = "Game directory path:";
             // 
@@ -112,10 +111,10 @@
             this.DisableLoadingScreensCheckBox.AutoSize = true;
             this.DisableLoadingScreensCheckBox.Checked = global::BNSBoost.Properties.Settings.Default.NoLoadingScreens;
             this.DisableLoadingScreensCheckBox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::BNSBoost.Properties.Settings.Default, "NoLoadingScreens", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.DisableLoadingScreensCheckBox.Location = new System.Drawing.Point(3, 36);
-            this.DisableLoadingScreensCheckBox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.DisableLoadingScreensCheckBox.Location = new System.Drawing.Point(8, 86);
+            this.DisableLoadingScreensCheckBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DisableLoadingScreensCheckBox.Name = "DisableLoadingScreensCheckBox";
-            this.DisableLoadingScreensCheckBox.Size = new System.Drawing.Size(138, 17);
+            this.DisableLoadingScreensCheckBox.Size = new System.Drawing.Size(356, 36);
             this.DisableLoadingScreensCheckBox.TabIndex = 19;
             this.DisableLoadingScreensCheckBox.Text = "Disable loading screens";
             this.DisableLoadingScreensCheckBox.UseVisualStyleBackColor = true;
@@ -125,10 +124,10 @@
             this.LauncherPathTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.LauncherPathTextBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", global::BNSBoost.Properties.Settings.Default, "NCLauncherRPath", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.LauncherPathTextBox.Location = new System.Drawing.Point(3, 75);
-            this.LauncherPathTextBox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.LauncherPathTextBox.Location = new System.Drawing.Point(8, 179);
+            this.LauncherPathTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.LauncherPathTextBox.Name = "LauncherPathTextBox";
-            this.LauncherPathTextBox.Size = new System.Drawing.Size(332, 20);
+            this.LauncherPathTextBox.Size = new System.Drawing.Size(879, 38);
             this.LauncherPathTextBox.TabIndex = 15;
             this.LauncherPathTextBox.Text = global::BNSBoost.Properties.Settings.Default.NCLauncherRPath;
             // 
@@ -138,10 +137,10 @@
             this.UseAllCoresCheckbox.Checked = global::BNSBoost.Properties.Settings.Default.UseAllCores;
             this.UseAllCoresCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
             this.UseAllCoresCheckbox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::BNSBoost.Properties.Settings.Default, "UseAllCores", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.UseAllCoresCheckbox.Location = new System.Drawing.Point(3, 1);
-            this.UseAllCoresCheckbox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.UseAllCoresCheckbox.Location = new System.Drawing.Point(8, 2);
+            this.UseAllCoresCheckbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.UseAllCoresCheckbox.Name = "UseAllCoresCheckbox";
-            this.UseAllCoresCheckbox.Size = new System.Drawing.Size(87, 17);
+            this.UseAllCoresCheckbox.Size = new System.Drawing.Size(216, 36);
             this.UseAllCoresCheckbox.TabIndex = 2;
             this.UseAllCoresCheckbox.Text = "Use all cores";
             this.UseAllCoresCheckbox.UseVisualStyleBackColor = true;
@@ -149,10 +148,9 @@
             // LauncherPathLabel
             // 
             this.LauncherPathLabel.AutoSize = true;
-            this.LauncherPathLabel.Location = new System.Drawing.Point(1, 61);
-            this.LauncherPathLabel.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.LauncherPathLabel.Location = new System.Drawing.Point(3, 145);
             this.LauncherPathLabel.Name = "LauncherPathLabel";
-            this.LauncherPathLabel.Size = new System.Drawing.Size(79, 13);
+            this.LauncherPathLabel.Size = new System.Drawing.Size(205, 32);
             this.LauncherPathLabel.TabIndex = 18;
             this.LauncherPathLabel.Text = "Launcher path:";
             this.LauncherPathLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
@@ -163,10 +161,10 @@
             this.DisableTextureStreamingCheckbox.Checked = global::BNSBoost.Properties.Settings.Default.NoTextureStreaming;
             this.DisableTextureStreamingCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
             this.DisableTextureStreamingCheckbox.DataBindings.Add(new System.Windows.Forms.Binding("Checked", global::BNSBoost.Properties.Settings.Default, "NoTextureStreaming", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
-            this.DisableTextureStreamingCheckbox.Location = new System.Drawing.Point(3, 19);
-            this.DisableTextureStreamingCheckbox.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.DisableTextureStreamingCheckbox.Location = new System.Drawing.Point(8, 45);
+            this.DisableTextureStreamingCheckbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.DisableTextureStreamingCheckbox.Name = "DisableTextureStreamingCheckbox";
-            this.DisableTextureStreamingCheckbox.Size = new System.Drawing.Size(144, 17);
+            this.DisableTextureStreamingCheckbox.Size = new System.Drawing.Size(375, 36);
             this.DisableTextureStreamingCheckbox.TabIndex = 2;
             this.DisableTextureStreamingCheckbox.Text = "Disable texture streaming";
             this.DisableTextureStreamingCheckbox.UseVisualStyleBackColor = true;
@@ -175,24 +173,24 @@
             // 
             this.MainTabControl.Controls.Add(this.GameTabPage);
             this.MainTabControl.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.MainTabControl.Location = new System.Drawing.Point(4, 4);
-            this.MainTabControl.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.MainTabControl.Location = new System.Drawing.Point(11, 10);
+            this.MainTabControl.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MainTabControl.Name = "MainTabControl";
             this.MainTabControl.SelectedIndex = 0;
-            this.MainTabControl.Size = new System.Drawing.Size(351, 188);
+            this.MainTabControl.Size = new System.Drawing.Size(935, 448);
             this.MainTabControl.TabIndex = 7;
             // 
             // BNSBoostForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(359, 225);
+            this.ClientSize = new System.Drawing.Size(957, 537);
             this.Controls.Add(this.MainTabControl);
             this.Controls.Add(this.LaunchButton);
-            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "BNSBoostForm";
-            this.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Padding = new System.Windows.Forms.Padding(11, 10, 11, 10);
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "BNSBoost";
             this.Load += new System.EventHandler(this.Form_Load);
