2980ce889c8f638ae498b13b27ae3890371f04f0
LoadLibraryW <LoadLibraryW@protonmail.com>
2017-04-29 22:00:56 -0400

    Updating the build would help

--- BNSBoost/Program.cs
+++ BNSBoost/Program.cs
@@ -6,6 +6,7 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Threading;
 using System.Diagnostics;
+using System.Reflection;
 
 namespace BNSBoost
 {
@@ -17,10 +18,28 @@ namespace BNSBoost
         [STAThread]
         static void Main()
         {
+            UseEmbeddedDependencies();
             //new BNSDat().Extract(@"D:\Blade and Soul\BnS\contents\Local\NCWEST\data\config.dat", false);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new BNSBoostForm());
+        }
+
+        static void UseEmbeddedDependencies()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, bargs) =>
+            {
+                String dllName = new AssemblyName(bargs.Name).Name + ".dll";
+                var assem = Assembly.GetExecutingAssembly();
+                String resourceName = assem.GetManifestResourceNames().FirstOrDefault(rn => rn.EndsWith(dllName));
+                if (resourceName == null) return null; // Not found, maybe another handler will find it
+                using (var stream = assem.GetManifestResourceStream(resourceName))
+                {
+                    Byte[] assemblyData = new Byte[stream.Length];
+                    stream.Read(assemblyData, 0, assemblyData.Length);
+                    return Assembly.Load(assemblyData);
+                }
+            };
         }
     }
 }
