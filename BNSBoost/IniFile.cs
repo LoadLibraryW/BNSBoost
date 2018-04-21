9d406632588b7b0ecb0b74c89e606aa99bf3539d
Tudor Brindus <xyene@dmoj.ca>
2018-04-21 00:43:18 -0400

    Functional multiclient support

--- BNSBoost/IniFile.cs
+++ BNSBoost/IniFile.cs
@@ -1,61 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace BNSBoost
 {
-    using System.IO;
-    using System.Reflection;
-    using System.Runtime.InteropServices;
-    using System.Text;
-
     // https://stackoverflow.com/a/14906422
-    namespace MyProg
+    class IniFile
     {
-        class IniFile 
+        readonly string Path;
+        readonly string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public IniFile(string IniPath = null)
         {
-            readonly string Path;
-            readonly string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName;
+        }
 
-            [DllImport("kernel32", CharSet = CharSet.Unicode)]
-            static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+        public string Read(string Key, string Section = null)
+        {
+            var RetVal = new StringBuilder(255);
+            GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
+            return RetVal.ToString();
+        }
 
-            [DllImport("kernel32", CharSet = CharSet.Unicode)]
-            static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+        public void Write(string Key, string Value, string Section = null)
+        {
+            WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+        }
 
-            public IniFile(string IniPath = null)
-            {
-                Path = new FileInfo(IniPath ?? EXE + ".ini").FullName;
-            }
+        public void DeleteKey(string Key, string Section = null)
+        {
+            Write(Key, null, Section ?? EXE);
+        }
 
-            public string Read(string Key, string Section = null)
-            {
-                var RetVal = new StringBuilder(255);
-                GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 255, Path);
-                return RetVal.ToString();
-            }
+        public void DeleteSection(string Section = null)
+        {
+            Write(null, null, Section ?? EXE);
+        }
 
-            public void Write(string Key, string Value, string Section = null)
-            {
-                WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
-            }
-
-            public void DeleteKey(string Key, string Section = null)
-            {
-                Write(Key, null, Section ?? EXE);
-            }
-
-            public void DeleteSection(string Section = null)
-            {
-                Write(null, null, Section ?? EXE);
-            }
-
-            public bool KeyExists(string Key, string Section = null)
-            {
-                return Read(Key, Section).Length > 0;
-            }
+        public bool KeyExists(string Key, string Section = null)
+        {
+            return Read(Key, Section).Length > 0;
         }
     }
 }
