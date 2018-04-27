70c104e441e136d590a0a84d4627cd612a8d390d
Tudor Brindus <xyene@dmoj.ca>
2018-04-27 14:40:24 -0400

    Finish UI integration for updates; #11

--- src/BNSBoost/Properties/Settings.Designer.cs
+++ src/BNSBoost/Properties/Settings.Designer.cs
@@ -310,5 +310,17 @@ namespace BNSBoost.Properties {
                 this["MultiClientEnabled"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
+        public bool CheckForUpdates {
+            get {
+                return ((bool)(this["CheckForUpdates"]));
+            }
+            set {
+                this["CheckForUpdates"] = value;
+            }
+        }
     }
 }
